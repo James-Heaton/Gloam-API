e8f7d7149de6cfbcfa617b2643017cd876bebda8
James Heaton <heaton1138@gmail.com>
2025-12-15 14:22:13 -0600

    Models Created, Init Migration, Models Registered in Django Admin

--- gloamapi/admin.py
+++ gloamapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Area, CharacterType, Trait, Character, CharacterTrait
 
-# Register your models here.
+admin.site.register(Area)
+admin.site.register(CharacterType)
+admin.site.register(Trait)
+admin.site.register(Character)
+admin.site.register(CharacterTrait)
--- gloamapi/models.py
+++ gloamapi/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Read only Models
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class CharacterType(models.Model):
+    name = models.CharField(max_length=50)
+    max_hp = models.IntegerField()
+    max_mp = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Trait(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+# CRUD Model
+
+class Character(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='characters')
+    name = models.CharField(max_length=50)
+    character_type = models.ForeignKey(CharacterType, on_delete=models.PROTECT, related_name='characters')
+    hp = models.IntegerField()
+    mp = models.IntegerField()
+    current_area = models.ForeignKey(Area, on_delete=models.PROTECT, related_name='characters')
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+# Many-to-Many Join Table
+
+class CharacterTrait(models.Model):
+    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='character_traits')
+    trait = models.ForeignKey(Trait, on_delete=models.CASCADE, related_name='character_traits')
+
+    class Meta:
+        unique_together = ('character', 'trait')
+
+    def __str__(self):
+        return f"{self.character.name} - {self.trait.name}"
