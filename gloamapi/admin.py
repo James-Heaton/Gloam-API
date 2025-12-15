e68fc22bdf340c818c3d2f4c50645ad82c99d9fa
James Heaton <heaton1138@gmail.com>
2025-12-15 16:18:10 -0600

    Added Seed Data, Update Character Model, Auto-Defaults for hp/mp/area

--- gloamapi/admin.py
+++ gloamapi/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Area, CharacterType, Trait, Character, CharacterTrait
 
+
+class CharacterAdmin(admin.ModelAdmin):
+    fields = ["user", "name", "character_type", "current_area", "is_active", "hp", "mp"]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not obj:  # Only for new objects
+            form.base_fields["hp"].required = False
+            form.base_fields["mp"].required = False
+            form.base_fields["current_area"].required = False
+        return form
+
+
 admin.site.register(Area)
 admin.site.register(CharacterType)
 admin.site.register(Trait)
-admin.site.register(Character)
+admin.site.register(Character, CharacterAdmin)
 admin.site.register(CharacterTrait)
--- gloamapi/models.py
+++ gloamapi/models.py
@@ -3,12 +3,14 @@ from django.contrib.auth.models import User
 
 # Read only Models
 
+
 class Area(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
     def __str__(self):
         return self.name
+
 
 class CharacterType(models.Model):
     name = models.CharField(max_length=50)
@@ -18,6 +20,7 @@ class CharacterType(models.Model):
     def __str__(self):
         return self.name
 
+
 class Trait(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
@@ -25,28 +28,67 @@ class Trait(models.Model):
     def __str__(self):
         return self.name
 
+
 # CRUD Model
 
+
 class Character(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='characters')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="characters")
     name = models.CharField(max_length=50)
-    character_type = models.ForeignKey(CharacterType, on_delete=models.PROTECT, related_name='characters')
+    character_type = models.ForeignKey(
+        CharacterType, on_delete=models.PROTECT, related_name="characters"
+    )
     hp = models.IntegerField()
     mp = models.IntegerField()
-    current_area = models.ForeignKey(Area, on_delete=models.PROTECT, related_name='characters')
+    current_area = models.ForeignKey(
+        Area, on_delete=models.PROTECT, related_name="characters"
+    )
     is_active = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        """Set HP, MP, and starting area on creation if not provided"""
+        if not self.pk:  # Only on creation
+            if not self.hp:
+                self.hp = self.character_type.max_hp
+            if not self.mp:
+                self.mp = self.character_type.max_mp
+            if not self.current_area_id:
+                self.current_area_id = 1
+        super().save(*args, **kwargs)
+
+    @property
+    def max_hp(self):
+        """Get max HP from character type"""
+        return self.character_type.max_hp
+
+    @property
+    def max_mp(self):
+        """Get max MP from character type"""
+        return self.character_type.max_mp
+
+    def reset_to_defaults(self):
+        """Reset character HP/MP to max values"""
+        self.hp = self.character_type.max_hp
+        self.mp = self.character_type.max_mp
+        self.save()
 
     def __str__(self):
         return self.name
 
+
 # Many-to-Many Join Table
 
+
 class CharacterTrait(models.Model):
-    character = models.ForeignKey(Character, on_delete=models.CASCADE, related_name='character_traits')
-    trait = models.ForeignKey(Trait, on_delete=models.CASCADE, related_name='character_traits')
+    character = models.ForeignKey(
+        Character, on_delete=models.CASCADE, related_name="character_traits"
+    )
+    trait = models.ForeignKey(
+        Trait, on_delete=models.CASCADE, related_name="character_traits"
+    )
 
     class Meta:
-        unique_together = ('character', 'trait')
+        unique_together = ("character", "trait")
 
     def __str__(self):
         return f"{self.character.name} - {self.trait.name}"
