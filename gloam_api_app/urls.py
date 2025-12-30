5cd3b815aa1b58c5f5d9f525ed498029878cd4b7
James Heaton <heaton1138@gmail.com>
2025-12-29 22:18:13 -0600

    Restructured serailizers / Added game_serializers and game_view

--- gloam_api_app/urls.py
+++ gloam_api_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views.auth import register_user, login_user
 from .views.character_views import character_list, character_detail
 from .views.reference_views import character_type_list, trait_list, area_list
+from .views.game_views import game_state, execute_game_action, reset_game
 
 urlpatterns = [
     # Auth
@@ -15,5 +16,10 @@ urlpatterns = [
 
     # Characters
     path("characters", character_list, name="character_list"),
-    path("characters/<int:character_id>", character_detail, name="character_detail")
+    path("characters/<int:character_id>", character_detail, name="character_detail"),
+
+    # Game
+    path("game/state", game_state, name="game_state"),
+    path("game/action", execute_game_action, name="execute_game_action"),
+    path("game/reset", reset_game, name="reset_game"),
 ]
